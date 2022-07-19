e3dd8552339ce73fd67e24d90c33e2c7f52d5fbc
TheJoeruV2 <jjb9762@nyu.edu>
2022-07-19 15:00:38 -0400

    Update GrapplingGun.cs

--- Cyber Swing/Assets/FPS_Movement_Rigidbody-master/GrapplingGun.cs
+++ Cyber Swing/Assets/FPS_Movement_Rigidbody-master/GrapplingGun.cs
@@ -6,7 +6,7 @@ public class GrapplingGun : MonoBehaviour {
     private Vector3 grapplePoint;
     public LayerMask whatIsGrappleable;
     public Transform gunTip, camera, player;
-    private float maxDistance = 100f;
+    private float maxDistance = 20f;
     private SpringJoint joint;
 
     void Awake() {
