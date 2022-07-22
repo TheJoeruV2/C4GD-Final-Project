5b66ee23ea208187b84e54c2039c20bdfd4bc91d
TheJoeruV2 <jjb9762@nyu.edu>
2022-07-22 11:34:17 -0400

    Menu and transitions

---
+++ Cyber Swing/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+   public void LoadScene(string MoreLevelsSoon)
+   {
+      SceneManager.LoadScene(MoreLevelsSoon);
+   }
+
+
+
+}
